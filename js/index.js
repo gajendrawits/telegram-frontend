ee164d2fa8583f29ee2e5254101b6f18f53a4fd6
Gajendra Mehra <93523171+gajendrawits@users.noreply.github.com>
2022-10-26 13:53:33 +0530

    prod url added

---
+++ js/index.js
@@ -0,0 +1,35 @@
+// document.querySelector('#locationData').innerHTML = initDataUnsafe.user.username;
+const frontEndUrl = "https://gajendrawits.github.io/telegram-frontend"
+const baseUrl = "https://staging.chapterx.network"
+
+function setThemeClass() {
+    document.documentElement.className = Telegram.WebApp.colorScheme;
+}
+Telegram.WebApp.onEvent('themeChanged', setThemeClass);
+setThemeClass();
+
+function webviewClose() {
+    Telegram.WebApp.close();
+}
+function copyText() {
+    let text = document.getElementById("displayMeetingText").value;
+    // alert(text)
+    navigator.clipboard.writeText(text)
+        .then(() => {
+            alert('Text copied to clipboard');
+        })
+        .catch(err => {
+            // alert('Error in copying text: ', err);
+        });
+}
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return pair[1];
+        }
+    }
+    alert('Query Variable ' + variable + ' not found');
+}
